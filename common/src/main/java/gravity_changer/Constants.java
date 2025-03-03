d0cf3791b9dd926a645e1fcbf1ae98214bc507a0
Tfarcenim <44327798+Tfarcenim@users.noreply.github.com>
2025-03-03 06:34:42 -0500

    begin forge port

---
+++ common/src/main/java/gravity_changer/Constants.java
@@ -0,0 +1,11 @@
+package gravity_changer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+	public static final String MOD_ID = "gravitychanger";
+	public static final String MOD_NAME = "GravityChanger";
+	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
